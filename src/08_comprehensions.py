ce82d2f7802dcbc21bc301fb1ea8cc536b39e366
Matthew Hidalgo <makeshifthidalgo@hotmail.com>
2020-08-31 15:24:43 -0700

    mods 1 - 10 complete

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -11,6 +11,8 @@ for more info regarding list comprehensions.
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
 y = []
+for i in range(5):
+    y.append(i + 1)
 
 print (y)
 
@@ -18,7 +20,8 @@ print (y)
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
 y = []
-
+for i in range(10):
+    y.append(i**3)
 print(y)
 
 # Write a list comprehension to produce the uppercase version of all the
@@ -26,7 +29,10 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
+
 y = []
+for word in a:
+    y.append(word.upper())
 
 print(y)
 
